7971bfe4891a27ba565c53040f9c7cca9deef180
senglk <senglk@users.noreply.github.com>
2020-09-27 21:30:04 +0800

    Create clock1414.py

---
+++ Linux/clock1414.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import serial
+import datetime
+import time
+
+timestr=''
+lastSent=''
+cont = True
+
+with serial.Serial('/dev/ttyUSB0', 19200, timeout=1) as ser:
+    # Sleep 1 second to let things stablise.
+    # If weird symbols appear instead, or time is chopped up
+    # Try increasing it to 2s
+    time.sleep(1)
+    try:
+        while cont:
+            time.sleep(1) # check time every 1 second
+            timestr = datetime.datetime.now().strftime('%H%M')
+            if lastSent == timestr:
+                continue # if HHMM same as last update, go to next loop iteration
+            bytesSent = ser.write(timestr.encode('utf-8'))
+            lastSent = timestr
+    except KeyboardInterrupt:
+            cont = False
+print('Exiting...')
